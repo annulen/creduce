f70de56e278ad0f2e250dfc66ecd42348ae62fef
Konstantin Tokarev <ktokarev@smartlabs.tv>
2013-01-02 19:49:52 +0400

    Fixed compilation with Clang 3.2

--- clang_delta/TransformationManager.cpp
+++ clang_delta/TransformationManager.cpp
@@ -127,7 +127,7 @@ bool TransformationManager::initializeCompilerInstance(std::string &ErrorMsg)
   PP.getBuiltinInfo().InitializeBuiltins(PP.getIdentifierTable(),
                                          PP.getLangOpts());
 
-  if (!ClangInstance->InitializeSourceManager(SrcFileName)) {
+  if (!ClangInstance->InitializeSourceManager(FrontendInputFile(SrcFileName, IK_None))) {
     ErrorMsg = "Cannot open source file!";
     return false;
   }
